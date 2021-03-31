0127712d5f21fd9b883117c6d45d289cfee1bfc1
John Mager <jmageriii@gmail.com>
2021-03-30 22:12:04 -0400

    Move comonad to own file

--- pkgs/development/idris2-packages/default.nix
+++ pkgs/development/idris2-packages/default.nix
@@ -5,30 +5,11 @@ let
     pkgs.callPackage ./buildIdris.nix
       { inherit src name withPackages idrisLibraries idris2; };
 
-  idrisPackages = rec {
-    comonad = buildIdris {
-      name = "comonad";
-      src = fetchFromGitHub {
-        owner = "stefan-hoeck";
-        repo = "idris2-comonad";
-        rev = "9bc8ae122160231b6c387e86bae0a34f34f678eb";
-        sha256 = "uJjGvZnmftkl4bwTXN9aOAXPEjsz/8QxHvyeA/gjDaw=";
-      };
-    };
+in
+rec {
+  packages = rec {
+    comonad = pkgs.callPackage ./comonad.nix { inherit buildIdris; };
   };
 
-in
-rec { 
-  packages = rec {
-    comonad = buildIdris {
-      name = "comonad";
-      src = fetchFromGitHub {
-        owner = "stefan-hoeck";
-        repo = "idris2-comonad";
-        rev = "9bc8ae122160231b6c387e86bae0a34f34f678eb";
-        sha256 = "uJjGvZnmftkl4bwTXN9aOAXPEjsz/8QxHvyeA/gjDaw=";
-      };
-    };
-  };
   withPkgs = fn: withPackages (fn packages);
 }
