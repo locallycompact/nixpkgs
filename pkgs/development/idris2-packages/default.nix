591dfb7d56ba6a32ea2f7bf7a337d0c2f872725e
John Mager <jmageriii@gmail.com>
2021-04-06 21:39:40 -0400

    example other call method

--- pkgs/development/idris2-packages/default.nix
+++ pkgs/development/idris2-packages/default.nix
@@ -4,6 +4,7 @@ let
   buildIdris = { name, src, idrisLibraries ? [ ], extraBuildInputs ? [ ] }:
     pkgs.callPackage ./buildIdris.nix
       { inherit src name withPackages extraBuildInputs idrisLibraries idris2; };
+  mythingy = file: args: pkgs.callPackage file (lib.recursiveUpdate { inherit buildIdris; } args);
 
 in
 rec {
@@ -12,6 +13,7 @@ rec {
     elab-util = pkgs.callPackage ./elab-util.nix { inherit buildIdris; };
     readline = pkgs.callPackage ./readline.nix { inherit buildIdris; };
     with-ffi = pkgs.callPackage ./with-ffi.nix { inherit buildIdris; };
+    comonado = mythingy ./comonad.nix { };
   };
 
   withPkgs = fn: withPackages (fn packages);
